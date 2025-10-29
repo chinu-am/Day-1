98e9404ba0bd2d5f63ad4732229c7f8b6ef67c92
chinu-am <hello123says@gmail.com>
2025-10-29 17:54:20 +0530

    Add files via upload

---
+++ CheckNumber.java
@@ -0,0 +1,30 @@
+class CheckNumbers {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = -5;
+        int num3 = 0;
+        if (num1 > 0) {
+            System.out.println(num1 + " is Positive");
+        } else if (num1 < 0) {
+            System.out.println(num1 + " is Negative");
+        } else {
+            System.out.println(num1 + " is Zero");
+        }
+
+        if (num2 > 0) {
+            System.out.println(num2 + " is Positive");
+        } else if (num2 < 0) {
+            System.out.println(num2 + " is Negative");
+        } else {
+            System.out.println(num2 + " is Zero");
+        }
+
+        if (num3 > 0) {
+            System.out.println(num3 + " is Positive");
+        } else if (num3 < 0) {
+            System.out.println(num3 + " is Negative");
+        } else {
+            System.out.println(num3 + " is Zero");
+        }
+    }
+}
