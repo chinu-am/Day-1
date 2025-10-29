24696b0beed22c57d2115a3c43c1acceb5c4218d
chinu-am <hello123says@gmail.com>
2025-10-29 16:55:08 +0530

    Add files via upload

---
+++ Company.java
@@ -0,0 +1,7 @@
+class Company {
+    public static void main(String[] args) {
+        String a = "Wipro";
+        String b = "Bangalore";
+        System.out.println(a + " Technologies " + b);
+    }
+}
